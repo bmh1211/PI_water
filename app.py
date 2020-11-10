f24fd5555a99e448484ae4367acf039e052f4d16
Louie Kim <hyeok0724.kim@gmail.com>
2020-11-10 14:43:32 +0900

    Set CORS

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, render_template
+from flask_cors import CORS,cross_origin
 
 app=Flask(__name__)
+CORS(app, support_credentials=True)
 g_btn_status = "None"
 
 @app.route('/')
